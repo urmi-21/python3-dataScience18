db74b0cbb25bf5dad3da64e6e5c9060e6392e5f1
urmi-21 <mrbains21@gmail.com>
2018-10-19 01:29:22 -0500

    ex

--- excercise/ex1.py
+++ excercise/ex1.py
@@ -5,13 +5,14 @@ Exercise 3
 @author urmi
 '''
 #1. Given a number, find its factorial i.e. n*n-1*n-2*...*1. Hint: use loop.
-n=5
+
+
 #Don't change this
 #Thanks Yuan Wang, BCB for the dict code
 geneticCode = {"UUU":"F", "UUC":"F", "UUA":"L", "UUG":"L",
               "UCU":"S", "UCC":"s", "UCA":"S", "UCG":"S",
               "UAU":"Y", "UAC":"Y", "UAG":"STOP",
-              "UGU":"C", "UGC":"C", "UGA":"STOP", "UGG":"W",
+              "UGU":"C", "UGC":"C", "UGA":"STOP","UAA":"STOP", "UGG":"W",
               "CUU":"L", "CUC":"L", "CUA":"L", "CUG":"L",
               "CCU":"P", "CCC":"P", "CCA":"P", "CCG":"P",
               "CAU":"H", "CAC":"H", "CAA":"Q", "CAG":"Q",
@@ -23,13 +24,13 @@ geneticCode = {"UUU":"F", "UUC":"F", "UUA":"L", "UUG":"L",
               "GUU":"V", "GUC":"V", "GUA":"V", "GUG":"V",
               "GCU":"A", "GCC":"A", "GCA":"A", "GCG":"A",
               "GAU":"D", "GAC":"D", "GAA":"E", "GAG":"E",
-              "GGU":"G", "GGC":"G", "GGA":"G", }
+              "GGU":"G", "GGC":"G", "GGA":"G","GGG":"G" }
 
 '''
 Above "geneticCode" is a dict which maps an RNA codon to an amino acid letter. Same from excercise 2.
 '''
 
-dnaCodons=['ATG','TTT','TTG','CTC','ATT','ATG','GTA','TAC','TAA','TAG','CAT','CAC','CAA','AAC','AAG','GAT','TGC','TGA','TGG','CGA','AGC','AGG','GGG']
+dnaCodons=['ATG','TTT','TTG','CTC','ATT','ATG','GTA','TAC','TAG','CAT','CAC','CAA','AAC','AAG','GAT','TGC','TGA','TGG','CGA','AGC','AGG','GGG']
 
 #2. dnaCodons is a list of valid codons. Using a for loop iterate over this list and print corresponding amino acid.
 #Hint: The dict geneticCode is defined for RNA codons you will have to first convert the given DNA codons to RNA codons. To this replace all 'T' with 'U'.
@@ -37,7 +38,7 @@ dnaCodons=['ATG','TTT','TTG','CTC','ATT','ATG','GTA','TAC','TAA','TAG','CAT','CA
 
 #3. If you did step 1 correctly you will notice two "STOP" in your amino acid sequence. Biologically, the "Stop" codons mark te end of a peptide.
 #Use an if condition to skip printing of the "Stop" codon. Can you do the same using a continue statement?
-#Hint: the codons that will result in a stop codon are "UAG" and "UGA".
+#Hint: the codons that will result in a stop codon are "UAG","UGA" or "UAA".
 
 '''
 *Bonus Question
@@ -55,4 +56,4 @@ for i in range(0,len(dnaSeq)-3,3):
         break
     if t == "ATG" or flag == True:
         flag=True
-        print geneticCode[t.replace('T','U')]
+        print (geneticCode[t.replace('T','U')])
